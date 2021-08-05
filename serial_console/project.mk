d4cfef17af51685dd94a02d304a806481db474f1
pavelkozh <pskozhevnikov@stud.eltech.ru>
2021-08-05 14:09:59 +0300

    Enter and display symbol

---
+++ serial_console/project.mk
@@ -0,0 +1,14 @@
+# C source files
+PROJECT_CSRC 	= src/main.c \
+		  src/callbacks.c
+				  
+# C++ source files
+PROJECT_CPPSRC 	= 
+# Directories to search headers in
+PROJECT_INCDIR	= incl
+# Additional libraries
+PROJECT_LIBS	=
+# Compiler options
+PROJECT_OPT     =
+
+# Additional .mk files are included here
---
+++ serial_console/src/main.c
@@ -0,0 +1,48 @@
+#include "ch.h"
+#include "hal.h"
+#include <string.h>
+
+uint8_t text_buff[50]={};
+uint8_t* ptr=text_buff;
+
+static SerialDriver *SDdriver = &SD2;
+
+static const SerialConfig sdconf = {
+  .speed = SERIAL_DEFAULT_BITRATE,//default speed = 38400
+  .cr1 = 0,
+  .cr2 = USART_CR2_STOP_1,
+  .cr3 = 0
+};
+
+static THD_WORKING_AREA(waReadText, 128);
+static THD_FUNCTION(readText, arg) {
+
+  (void)arg;
+  while (1)
+  {
+	sdRead(SDdriver,ptr,1);
+    chThdSleepSeconds(1);
+	sdWrite(SDdriver,ptr,1);
+
+  }
+}
+
+int main(void) {
+
+	int8_t* str_pointer="Enter symbol:\r\n";
+
+    halInit();
+    chSysInit();
+    sdInit();
+    sdStart(SDdriver,&sdconf);
+    chThdCreateStatic(waReadText, sizeof(waReadText), NORMALPRIO+3, readText, NULL);
+    palSetPadMode(GPIOA,GPIOA_LED_GREEN,PAL_MODE_OUTPUT_PUSHPULL);
+    sdWrite(SDdriver,str_pointer,strlen(str_pointer));
+
+    while (1) {
+
+    	palToggleLine(LINE_LED_GREEN);
+        chThdSleepMilliseconds(500);
+
+    }
+}
